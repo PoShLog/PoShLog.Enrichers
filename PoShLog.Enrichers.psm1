01d827c598e2f43597ec3edbda941c6f87d89781
Tomáš Bouda <email@tomasbouda.cz>
2020-03-28 13:50:23 +0100

    init

---
+++ PoShLog.Enrichers.psm1
@@ -0,0 +1,19 @@
+[string] $Global:packagesPath = "$PSScriptRoot\lib"
+
+# dot source all script files
+Get-ChildItem -Path "$PSScriptRoot\functions" -Recurse -File -Filter '*.ps1' | ForEach-Object {
+	. $_.FullName
+
+	# Export all functions except internal
+	if ($_.FullName -notcontains 'internal') {
+		Export-ModuleMember $_.BaseName
+	}
+}
+
+# Load all package dlls
+try {
+	Get-ChildItem $Global:packagesPath | Where-Object { $_.Name -like '*.dll' } | ForEach-Object { Add-Type -Path $_ -ErrorAction Stop }
+}
+catch {
+	Write-Error ($_.Exception.GetBaseException().LoaderExceptions | Format-Table | Out-String)
+}
